cbe702bdb128f588278d428cd5fab4a255e8158d
German Monti Rubio <german.monti@pinapp.tech>
2025-06-05 00:05:16 -0300

    User response y request de update

--- src/main/java/com/grup14/luterano/response/UserUpdateResponse.java
+++ src/main/java/com/grup14/luterano/response/UserUpdateResponse.java
@@ -1,19 +1,20 @@
-package com.grup14.luterano.request;
+package com.grup14.luterano.response;
 
 import com.grup14.luterano.entities.Role;
 import com.grup14.luterano.entities.enums.UserStatus;
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter@Setter@Builder
-public class UserUpdateRequest {
+@Getter
+@Setter
+@Builder
+public class UserUpdateResponse {
     @NotNull(message = "El mail es un campo obligatorio")
     private String email;
-    private String emailNuevo;
-    private String password;
     private Role rol;
     private UserStatus userStatus;
+    private String mensaje;
+    private Integer code;
 }
