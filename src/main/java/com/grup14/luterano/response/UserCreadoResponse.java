17c753431ecbfeeb2056911785dd2f044aa8b181
German Monti Rubio <german.monti@pinapp.tech>
2025-06-05 00:04:29 -0300

    Se agrega userResponse

---
+++ src/main/java/com/grup14/luterano/response/UserCreadoResponse.java
@@ -0,0 +1,18 @@
+package com.grup14.luterano.response;
+
+import com.grup14.luterano.entities.Role;
+import com.grup14.luterano.entities.enums.UserStatus;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserCreadoResponse {
+    private String email;
+    private Role role;
+    private UserStatus userStatus;
+    private String mensaje;
+    private Integer code;
+}
