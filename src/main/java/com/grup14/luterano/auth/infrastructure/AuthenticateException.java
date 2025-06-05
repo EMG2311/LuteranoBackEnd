ee863c100a5359d36c008c9200f47b007f9d2be5
German Monti Rubio <german.monti@pinapp.tech>
2025-06-05 00:02:28 -0300

    Se cambia Exception por RuntimeException

--- src/main/java/com/grup14/luterano/auth/infrastructure/AuthenticateException.java
+++ src/main/java/com/grup14/luterano/auth/infrastructure/AuthenticateException.java
@@ -2,7 +2,7 @@ package com.grup14.luterano.auth.infrastructure;
 
 import org.apache.logging.log4j.message.Message;
 
-public class AuthenticateException extends Exception{
+public class AuthenticateException extends RuntimeException{
     public String mensajeNuevo;
     public AuthenticateException(String mensaje){
         mensajeNuevo=mensaje;
