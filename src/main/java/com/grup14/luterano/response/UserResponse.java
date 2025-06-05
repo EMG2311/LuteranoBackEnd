a607832ff2944ffd830bf7042d454ca3595007a0
German Monti Rubio <german.monti@pinapp.tech>
2025-06-05 00:04:19 -0300

    Se agregan campos de mensaje y code

--- src/main/java/com/grup14/luterano/response/UserResponse.java
+++ src/main/java/com/grup14/luterano/response/UserResponse.java
@@ -1,13 +1,20 @@
 package com.grup14.luterano.response;
 
+
 import com.grup14.luterano.entities.Role;
 import com.grup14.luterano.entities.enums.UserStatus;
 import lombok.*;
 
-@Builder@NoArgsConstructor@Getter@Setter
+@Builder
+@NoArgsConstructor
+@Getter
+@Setter
 @AllArgsConstructor
 public class UserResponse {
     private String email;
     private Role role;
     private UserStatus userStatus;
+    private String mensaje;
+    private Integer code;
+
 }
