0719e0924c7196fe7abf132637d0a40b83eae912
German Monti Rubio <german.monti@pinapp.tech>
2025-06-05 00:05:27 -0300

    Se agregan para swagger

--- src/main/java/com/grup14/luterano/config/WebSecurityConfig.java
+++ src/main/java/com/grup14/luterano/config/WebSecurityConfig.java
@@ -27,7 +27,10 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("api/auth/**").permitAll()
+                        .requestMatchers("api/auth/**"
+                                ,"/swagger-ui/**"
+                                ,"/swagger-ui.html"
+                                ,"/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
