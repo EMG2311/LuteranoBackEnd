77bb39a10be8704e3c8a87db29334f2228b1aeee
German <germanmontirubio@gmail.com>
2025-06-16 15:31:04 -0300

    Se genera usuario admin por defecto

--- src/main/java/com/grup14/luterano/DataInitializer.java
+++ src/main/java/com/grup14/luterano/DataInitializer.java
@@ -1,10 +1,14 @@
 package com.grup14.luterano;
 
 import com.grup14.luterano.entities.Role;
+import com.grup14.luterano.entities.User;
+import com.grup14.luterano.entities.enums.UserStatus;
 import com.grup14.luterano.repository.RoleRepository;
+import com.grup14.luterano.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -12,7 +16,7 @@ import java.util.List;
 public class DataInitializer {
 
     @Bean
-    CommandLineRunner initRoles(RoleRepository roleRepository) {
+    CommandLineRunner initRoles(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
             List<String> roles = List.of(
                     "ROLE_ADMIN",
@@ -31,6 +35,15 @@ public class DataInitializer {
                             return roleRepository.save(role);
                         });
             }
+            userRepository.findByEmail("admnin@gmail.com").orElseGet(()->
+                 userRepository.save(User.builder()
+                                .email("admnin@gmail.com")
+                                .password(passwordEncoder.encode("44575808"))
+                    .userStatus(UserStatus.CREADO)
+                            .rol(roleRepository.findByName("ROLE_ADMIN").get())
+                            .build())
+            );
+
 
             System.out.println("✅ Roles inicializados correctamente");
         };
