25548df7d7256cf8291ce047af79ff023de624e7
german <germanmontirubio@gmail.com>
2025-07-03 17:12:02 -0300

    Se modifica configuracion de cof

--- src/main/java/com/grup14/luterano/config/WebSecurityConfig.java
+++ src/main/java/com/grup14/luterano/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,6 +27,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults()) // << AGREGAR ESTA LÍNEA
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("api/auth/**"
                                 ,"/swagger-ui/**"
