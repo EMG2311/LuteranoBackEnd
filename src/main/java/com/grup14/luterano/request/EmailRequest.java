dcdccdf50c0baad8df84cac9010dbe4bf5026e8e
german <germanmontirubio@gmail.com>
2025-07-06 20:12:21 -0300

    Validacion de EmailRequest

--- src/main/java/com/grup14/luterano/request/EmailRequest.java
+++ src/main/java/com/grup14/luterano/request/EmailRequest.java
@@ -1,10 +1,12 @@
 package com.grup14.luterano.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter@Setter@NoArgsConstructor
 public class EmailRequest {
+    @NotBlank(message = "El mail no puede estar vacio")
     private String email;
 }
