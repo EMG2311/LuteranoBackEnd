ed3ed638628653a688d18da86a04b17d1bbe5469
german <germanmontirubio@gmail.com>
2025-07-28 20:51:25 -0300

    Se corrije el builder

--- src/main/java/com/grup14/luterano/response/docente/DocenteResponse.java
+++ src/main/java/com/grup14/luterano/response/docente/DocenteResponse.java
@@ -4,6 +4,7 @@ import com.grup14.luterano.commond.Persona;
 import com.grup14.luterano.commond.PersonaDto;
 import com.grup14.luterano.dto.DocenteDto;
 import com.grup14.luterano.entities.Materia;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,7 +12,8 @@ import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
-@SuperBuilder@Data
+@Builder
+@Data
 public class DocenteResponse {
     private DocenteDto docente;
     private Integer code;
