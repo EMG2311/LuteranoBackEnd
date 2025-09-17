133189590b4f4aa035637bd7124d17c983ca74a8
german <germanmontirubio@gmail.com>
2025-09-17 19:51:16 -0300

    Se cambian entidades

--- src/main/java/com/grup14/luterano/dto/CalificacionDto.java
+++ src/main/java/com/grup14/luterano/dto/CalificacionDto.java
@@ -1,25 +1,17 @@
 package com.grup14.luterano.dto;
 
-import lombok.AllArgsConstructor;
+import com.grup14.luterano.entities.HistorialMateria;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class CalificacionDto {
     private Long id;
     private float nota;
     private int numeroNota;
-    private float PG;
     private LocalDate fecha;
-
-    private AlumnoDto alumno;
-    private MateriaDto materia;
-    private CicloLectivoDto cicloLectivo;
-    private HistorialMateriaDto historialMaterias;
+    private HistorialMateriaDto historialMateria;
 }
