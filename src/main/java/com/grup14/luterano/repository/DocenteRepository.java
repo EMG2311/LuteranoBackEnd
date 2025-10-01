d635e5586ddfc602a6b99023173dea022e008126
german <germanmontirubio@gmail.com>
2025-09-30 21:55:16 -0300

    Se hace delete soft en docente y preceptor

--- src/main/java/com/grup14/luterano/repository/DocenteRepository.java
+++ src/main/java/com/grup14/luterano/repository/DocenteRepository.java
@@ -4,9 +4,16 @@ import com.grup14.luterano.entities.Docente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 @Repository
 public interface DocenteRepository extends JpaRepository<Docente,Long> {
     Optional<Docente> findByEmail(String email);
     Optional<Docente> findByDni(String dni);
+    Optional<Docente> findByEmailAndActiveIsTrue(String email);
+    Optional<Docente> findByDniAndActiveIsTrue(String dni);
+    Optional<Docente> findByEmailAndActiveIsFalse(String email);
+    Optional<Docente> findByIdAndActiveIsTrue(Long id);
+    List<Docente> findByActiveIsTrue();
+
 }
